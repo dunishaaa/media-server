beca4819c363bafb8d50e1d24b218660baed9485
dunishaaa <aphimontes@outlook.com>
2026-06-02 14:55:15 -0600

    Add search capabilities

--- frontend/src/components/FileInfo.tsx
+++ frontend/src/components/FileInfo.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import "../styles/fileContainer.css"
+import "../styles/searchBar.css"
 
 interface File{
     name: string
@@ -8,8 +9,21 @@ interface File{
     path: string
     modified: string
 }
-function FileContainer(props: {folder: string, file: File}){
 
+function SearchBar(props: {updateFilter: React.Dispatch<React.SetStateAction<string>>}){
+    const [searchText, setSearchText] = useState("");
+    function handleSearchChange(e: {target: {value: string}}){
+        setSearchText(e.target.value);
+        props.updateFilter(searchText);
+    }
+    return(
+        <div className="search-bar-container">
+            <input value={searchText} onChange={handleSearchChange} className="search-bar" type="text" placeholder="Search files..."></input>
+        </div>
+    );
+}
+
+function FileContainer(props: {folder: string, file: File}){
     let file = props.file;
     return(
         <div className="files-container">
@@ -25,8 +39,40 @@ function FileContainer(props: {folder: string, file: File}){
 
     )
 }
-function FolderList(props: {folderName:string}){
+
+function FileList(props: {files: File[], folderName:string, filter: string}){
+    console.log(`Filter from fileList: ${props.filter}`);
+    let filteredFiles = props.files.filter((file)=> file.name.toLowerCase().includes(props.filter));
+    if(filteredFiles.length == 0){
+        return (
+            <>
+                {
+                    props.files.map((file) =>
+                    <FileContainer key={file.name} file={file} folder={props.folderName}/>
+                )}
+            </>
+        )
+    }else{
+        return(
+            <>
+                {
+                    filteredFiles
+                    .map((file) =>
+                        <FileContainer key={file.name} file={file} folder={props.folderName}/>
+                    )
+                }
+            </>
+        )
+    }
+    
+}
+//Corregir : no queremos que llame a la api cada que se modifica el filtro, 
+// solo queremos que se filtre cada que se modifica el filtro
+// solo busca hasta que hayd dos segundo caracter?
+function FileInfo(props: {folderName: string}){
+    const [searchFilter, setSearchFilter] = useState("");
     const [files, setFiles] = useState<File[]>([]);
+
     useEffect(() => {
         async function fetchFileInfo(){
             try{
@@ -43,26 +89,11 @@ function FolderList(props: {folderName:string}){
         fetchFileInfo();
     }, []);
     console.log(files);
-    return (
-        <>
-            {files?.map((file) =>
-                <FileContainer key={file.name} file={file} folder={props.folderName}/>
-            )}
-        </>
-    )
-}
-function SearchBar(){
-    return(
-        <div>
-            <input className="search-bar" type="text" placeholder="Search files..."></input>
-        </div>
-    );
-}
-function FileInfo(props: {folderName: string}){
+
     return(
         <>
-            <SearchBar/>
-            <FolderList folderName={props.folderName}/>
+            <SearchBar updateFilter={setSearchFilter}/>
+            <FileList files={files} filter={searchFilter} folderName={props.folderName}/>
         </>
     )
 }
